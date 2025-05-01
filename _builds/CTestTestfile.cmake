0b4b6c1c0cc2a897f732600c8d19eec0ac450cab
matveech99 <matveech99@gmail.com>
2025-05-01 12:48:14 +0300

    lab07 final

---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/matvey/matveech99/workspace/projects/lab07
+# Build directory: /home/matvey/matveech99/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/matvey/matveech99/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/matvey/matveech99/workspace/projects/lab07/CMakeLists.txt;61;add_test;/home/matvey/matveech99/workspace/projects/lab07/CMakeLists.txt;0;")
